d61ee8a02e5b476affd87c1d9b3362a21859a212
BHariKrishnaReddy <reddyfrn007@gmail.com>
2022-01-26 23:48:12 +0530

    Improvised

--- routes.py
+++ routes.py
@@ -5,11 +5,11 @@ Created on Mon Jan 24 09:16:12 2022
 
 @author: harikrishnareddy
 """
-
-from flask import Flask,render_template
+import __init__.py
+from flask import render_template
 from flask import redirect,url_for
 
-app = Flask(__name__) #constructer
+
 
 @app.route("/<username>")
 def hello(username):
@@ -30,7 +30,3 @@ def greet_user(un):
     return redirect(url_for('evadu kavalira niku ',username=un))
 
 
-
-
-if __name__ == '__main__':
-    app.run(debug =True,port = 5001)
